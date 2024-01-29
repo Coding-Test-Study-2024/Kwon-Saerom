002bf0ef85ba503a141f54a9b004e41b6db4bf46
kwon nonna <89887999+NonnaKwon@users.noreply.github.com>
2024-01-29 13:30:30 +0900

    백준 4779 문제 풀이

---
+++ Week-01/Week-01/BOJ-4779.cpp
@@ -0,0 +1,37 @@
+﻿#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+string input;
+
+void Divide(int start, int end, char c)
+{
+	for (int i = start; i < end; i++)
+		input[i] = c;
+	if (end - start <= 1)
+		return;
+	if (c == ' ')
+		return;
+	int divideSize = (end - start) / 3;
+	Divide(start, start + divideSize, '-');
+	Divide(start + divideSize, start + 2 * divideSize, ' ');
+	Divide(start + 2 * divideSize, end, '-');
+}
+
+int main()
+{
+	while (true)
+	{
+		int N;
+		cin >> N;
+		input = "";
+		int count = pow(3, N);
+		for(int i=0;i<count;i++)
+			input += '-';
+		Divide(0, count, '-');
+		cout << input << endl;
+	}
+
+}
