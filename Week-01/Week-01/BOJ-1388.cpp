31acc47890d0ddc77d76d3c7f17a14abf10636b6
kwon nonna <89887999+NonnaKwon@users.noreply.github.com>
2024-01-29 14:44:32 +0900

    백준 1388 풀이

---
+++ Week-01/Week-01/BOJ-1388.cpp
@@ -0,0 +1,57 @@
+﻿#include <vector>
+#include <iostream>
+using namespace std;
+
+string input;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	//입력
+	int N, M;
+	cin >> N >> M;
+	vector<vector<char>> inputArr;
+	vector<vector<bool>> isVisited;
+	for (int i = 0; i < N; i++)
+	{
+		vector<char> tmp(M);
+		for (int j = 0; j < M; j++)
+		{
+			cin >> tmp[j];
+		}
+		inputArr.push_back(tmp);
+		isVisited.push_back(vector<bool> (M, false));
+	}
+
+	int answer = 0;
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			if (!isVisited[i][j])
+			{
+				answer++;
+				int tmpI = i;
+				while (tmpI < N && inputArr[tmpI][j] == '|')
+				{
+					isVisited[tmpI][j] = true;
+					tmpI++;
+				}
+
+				while (j < M && inputArr[i][j] == '-')
+				{
+					isVisited[i][j] = true;
+					j++;
+				}
+				j--;
+			}
+		}
+	}
+
+	cout << answer << endl;
+	
+}
