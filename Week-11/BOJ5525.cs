3dbe36cf07a7842938356eb650aec5da0b9efaaf
kwon nonna <89887999+NonnaKwon@users.noreply.github.com>
2024-10-17 17:56:21 +0900

    1번, 4번 못 품 / 3번 50점

---
+++ Week-11/BOJ5525.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CordingStudy_CS.Week11_CS
+{
+    internal class BOJ5525
+    {
+        static int N;
+        static int M;
+        static string S;
+        public static void Main5525(string[] args)
+        {
+            N = int.Parse(Console.ReadLine());
+            M = int.Parse(Console.ReadLine());
+            S = Console.ReadLine();
+
+            int count = 0;
+            for(int i=0;i<M;i++)
+            {
+                if (S[i] == 'I')
+                    if (IsIO(i) == true)
+                        count++;
+            }
+            Console.WriteLine(count);
+        }
+
+        public static bool IsIO(int startIndex)
+        {
+            int endIndex = startIndex + 2 * N + 1;
+            
+            if (endIndex > M)
+                return false;
+
+            int tmp = 0;
+            char bc = 'I';
+            for (int i = startIndex + 1; i < endIndex; i++)
+            {
+                if (bc == 'O' && S[i] == 'I')
+                {
+                    if (tmp == N)
+                        return true;
+                }
+                else if (bc == 'I' && S[i] == 'O')
+                {
+                    tmp++;
+                }
+                else
+                    return false;
+                bc = S[i];
+            }
+            return false;
+        }
+    }
+}
---
+++ Week-11/BOJ6064.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CordingStudy_CS.Week11_CS
+{
+    internal class BOJ6064
+    {
+        public static void Main6064(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            
+            while(T-->0)
+            {
+                string[] inputs = Console.ReadLine().Split(' ');
+                int M = int.Parse(inputs[0]);
+                int N = int.Parse(inputs[1]);
+                int x = int.Parse(inputs[2]);
+                int y = int.Parse(inputs[3]);
+
+                int answer = 0;
+                // x가 되기 위해서는 1~M 까지 x번 돌아야한다.
+                
+            }
+
+
+        }
+    }
+}
