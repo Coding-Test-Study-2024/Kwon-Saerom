cd3a485c8765ffb3638e3288fcd6e36261dd6476
kwon nonna <89887999+NonnaKwon@users.noreply.github.com>
2024-11-14 20:00:46 +0900

    Add files via upload

---
+++ Week-14/BOJ16202.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingStudy_CS.Week_14
+{
+    using tu = Tuple<int, int>;
+    internal class BOJ16202
+    {
+        static List<List<tu>> g = new List<List<tu>>();
+        public static void Main16202(string[] args)
+        {
+            // 일단 입력받기
+            string[] input = Console.ReadLine().Split(' ');
+            int N = int.Parse(input[0]);
+            int M = int.Parse(input[1]);
+            int K = int.Parse(input[2]);
+
+            for (int i = 0; i < N; i++)
+                g.Add(new List<tu>());
+            for(int i=0;i<M;i++)
+            {
+                string[] input2 = Console.ReadLine().Split(' ');
+                int x = int.Parse(input2[0]);
+                int y = int.Parse(input2[1]);
+                g[x - 1].Add(new tu(y - 1, i + 1));
+            }
+
+            //BFS로 방문을 모두 해서 최소인 것들로 갱신.
+            //만약 모두 방문이 안되면 0 넣기
+            List<int> answer = MST(K);
+            for(int i=0;i<answer.Count;i++)
+                Console.WriteLine(answer[i]);
+        }
+
+        static List<int> MST(int K)
+        {
+            List<int> answer = new List<int>();
+            while(K-->0)
+            {
+                int num = BFS();
+                if(num == 0)
+                {
+                    while (K-- > 0)
+                        answer.Add(0);
+                    break;
+                }
+                answer.Add(num);
+            }
+            return answer;
+        }
+        static int BFS()
+        {
+            Queue<tu> q = new Queue<tu>();
+
+            // 방문 리스트 초기화하기
+            List<int> isMin = new List<int>();
+            for (int i = 0; i < g.Count; i++)
+                isMin.Add(int.MaxValue);
+
+            // BFS 시작
+            if (g[0].Count == 0)
+            {
+                DeleteMin();
+                return 0;
+            }
+
+            q.Enqueue(new tu(g[0][0].Item1, g[0][0].Item2));
+            isMin[g[0][0].Item1] = g[0][0].Item2;
+
+            while(q.Count != 0)
+            {
+                tu t = q.Peek();
+                for(int i = 0; i < g[t.Item1].Count;i++)
+                {
+                    int dest = g[t.Item1][i].Item1;
+                    int weight = g[t.Item1][i].Item2;
+                    q.Enqueue(new tu(dest, weight));
+                    if (isMin[dest] > weight)
+                        isMin[dest] = weight;
+                }
+            }
+
+            DeleteMin();
+
+            // 모두 더하기
+            int sum = 0;
+            for (int i = 0; i < isMin.Count; i++)
+            {
+                if (isMin[i] == int.MaxValue)
+                {
+                    sum = 0;
+                    break;
+                }
+                sum += isMin[i];
+            }
+            return sum;
+        }
+
+        static void DeleteMin()
+        {
+
+        }
+    }
+}
---
+++ Week-14/BOJ25343.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingStudy_CS.Week_14
+{
+    internal class BOJ25343
+    {
+        static List<List<int>> arr = new List<List<int>>();
+        static List<List<int>> bList = new List<List<int>>();
+
+        // 오른쪽, 아래
+        static int[] moveX = { 1,0};
+        static int[] moveY = { 0,1};
+
+        static int N;
+        static int Max = 0;
+
+        public static void Main25343(string[] args)
+        {
+            // 입력을 받는다.
+            N = int.Parse(Console.ReadLine());
+            for(int i=0;i<N;i++)
+            {
+                List<int> tmp = new List<int>();
+                string[] input = Console.ReadLine().Split(' ');
+                for(int j=0;j<N;j++)
+                {
+                    tmp.Add(int.Parse(input[j]));
+                }
+                arr.Add(tmp);
+            }
+
+            // 최장증가부분수열 알고리즘 이용하여 푼다.
+            // DFS로 깊이우선 탐색
+            List<int> b = new List<int>();
+            b.Add(arr[0][0]);
+            DFS(0,0,b);
+
+            Console.WriteLine(Max);
+        }
+
+        static void DFS(int x,int y,List<int> b)
+        {
+            if (x < 0 || y < 0 || x >= N || y >= N)
+                return;
+
+            int removeIndex = 0;
+            int beforeValue = 0;
+            if (b[b.Count-1] < arr[y][x])
+            {
+                b.Add(arr[y][x]);
+                removeIndex = b.Count - 1;
+            }
+            else
+            {
+                int a = BinarySearch(b,arr[y][x]);
+                beforeValue = b[a];
+                removeIndex = a;
+
+                b[a] = arr[y][x];
+            }
+
+            if (x == N - 1 && y == N - 1)
+            {
+                if (b.Count > Max)
+                    Max = b.Count;
+                ReturnB(b, removeIndex, beforeValue);
+                return;
+            }
+
+            for (int i=0;i<moveX.Length;i++)
+            {
+                DFS(x + moveX[i], y + moveY[i], b);
+            }
+            ReturnB(b, removeIndex, beforeValue);
+        }
+
+        static int BinarySearch(List<int> b,int target)
+        {
+            int r = b.Count;
+            int l = 0;
+
+            while(l<r)
+            {
+                int m = (r + l) / 2;
+                if (b[m] < target)
+                    l = m + 1;
+                else if (b[m] > target)
+                    r = m;
+                else
+                    return m;
+            }
+
+            return r;
+        }
+
+        static void ReturnB(List<int> b,int index, int value)
+        {
+            if (index == b.Count - 1 && value == 0)
+                b.RemoveAt(index);
+            else
+                b[index] = value;
+        }
+    }
+}
